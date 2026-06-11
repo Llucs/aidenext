1a7b03132f3e71467f5c3d33db6439ec622cba65
Llucs <c307lucas@gmail.com>
2026-06-11 19:34:15 +0000

    fix(ci): use tasks.withType<KotlinCompile> instead of kotlin {} no root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,7 +81,7 @@ subprojects {
     }
   }
 
-  kotlin {
+  tasks.withType<KotlinCompile>().configureEach {
     compilerOptions {
       jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.fromTarget(BuildConfig.javaVersion.toString())
       freeCompilerArgs.add("-Xstring-concat=inline")
