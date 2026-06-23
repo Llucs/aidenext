098042a9c64d3de97ad041dae8e380f002750392
Llucs <c307lucas@gmail.com>
2026-06-23 09:57:05 +0000

    fix: revert to AGP 9.3.0-alpha12 for Gradle 9.x platform transform compat

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,8 +42,6 @@ buildscript {
   dependencies {
     classpath(libs.kotlin.gradle.plugin)
     classpath(libs.nav.safe.args.gradle.plugin)
-    classpath("org.ow2.asm:asm:9.9")
-    classpath("org.ow2.asm:asm-commons:9.9")
   }
 }
 
