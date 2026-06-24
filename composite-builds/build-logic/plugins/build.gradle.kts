383739ae18faa90d329c6633adc8aefd34cd69a0
Llucs <c307lucas@gmail.com>
2026-06-24 05:21:52 +0000

    fix: add ASM 9.7.1 directly to build-logic implementation

--- composite-builds/build-logic/plugins/build.gradle.kts
+++ composite-builds/build-logic/plugins/build.gradle.kts
@@ -43,15 +43,8 @@ dependencies {
   implementation(libs.common.antlr4)
   implementation(libs.google.gson)
   implementation(libs.google.java.format)
-
-  constraints {
-    implementation("org.ow2.asm:asm:9.7.1") {
-      because("AGP 9.2.1 requires ASM9 API level which needs ASM >= 9.0")
-    }
-    implementation("org.ow2.asm:asm-commons:9.7.1") {
-      because("AGP 9.2.1 requires ASM9 API level which needs ASM >= 9.0")
-    }
-  }
+  implementation("org.ow2.asm:asm:9.7.1")
+  implementation("org.ow2.asm:asm-commons:9.7.1")
 }
 
 gradlePlugin {
