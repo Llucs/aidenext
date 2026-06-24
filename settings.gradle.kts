66a73f5135644905957d8bcb5f9effe9f5730a2a
Llucs <c307lucas@gmail.com>
2026-06-24 00:38:49 +0000

    fix: add gradle libs-releases repo for tooling-api
    Co-authored-by: Llucs <c307lucas@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -72,6 +72,7 @@ dependencyResolutionManagement {
     maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
     maven { url = uri("https://s01.oss.sonatype.org/content/groups/public/") }
     maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
   }
 }
 
