a9c5496da51d729f6a4783baf66fa74d620ba5e4
Llucs <c307lucas@gmail.com>
2026-06-24 18:33:07 +0000

    fix: use Directory type for assets.directories.add instead of File

--- testing/benchmarks/build.gradle.kts
+++ testing/benchmarks/build.gradle.kts
@@ -30,7 +30,7 @@ extensions.configure<com.android.build.api.dsl.LibraryExtension> {
 
   sourceSets {
     getByName("androidTest") {
-      assets.directories.add(rootProject.file("utilities/framework-stubs/libs"))
+      assets.directories.add(rootProject.layout.projectDirectory.dir("utilities/framework-stubs/libs"))
     }
   }
 
